512080ce5e09b6b85479fce56eea4b13eb17c371
AdrianIsCoolio <89205176+AdrianIsCoolio@users.noreply.github.com>
2021-10-20 08:02:47 -0600

    is a long boi dragon
    ?

---
+++ src/AsciiArt.java
@@ -0,0 +1,17 @@
+public class AsciiArt {
+
+    public static void main(String[] args) {
+
+        System.out.println("          ((   ))");
+        System.out.println("           :: ::");
+        System.out.println("       ====)_^_(====");
+        System.out.println("      ===/ O < /:==");
+        System.out.println("     = | /_ _/ | =");
+        System.out.println("   =   //_ _//   =");
+        System.out.println("       /_ _/");
+        System.out.println("      (o_o)");
+        System.out.println("       UwU");
+        System.out.println("the Long Boi dragon");
+
+    }
+}
